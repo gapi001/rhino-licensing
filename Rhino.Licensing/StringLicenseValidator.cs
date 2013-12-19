c3fc3fa57e131e306ba4abe2d71325d4dfce86bc
gapi001 <gabrielpichard@hotmail.com>
2013-12-19 08:28:37 -0500

    Update StringLicenseValidator.cs
    Added a constructor to be able to set the discovery mode on the base class AbstractLicenseValidator

--- Rhino.Licensing/StringLicenseValidator.cs
+++ Rhino.Licensing/StringLicenseValidator.cs
@@ -5,6 +5,19 @@
     /// </summary>
     public class StringLicenseValidator : AbstractLicenseValidator
     {
+        
+        /// <summary>
+        /// Creates a new instance of <seealso cref="StringLicenseValidator"/>
+        /// </summary>
+        /// <param name="publicKey">public key</param>
+        /// <param name="license">license content</param>
+        /// <param name="IsDiscoveryHostMode">Activate DiscoveryMode to check for license on the shared network</param>
+        public StringLicenseValidator(string publicKey, string license, bool IsDiscoveryHostMode)
+            : base(publicKey,IsDiscoveryHostMode)
+        {
+            License = license;
+        }
+        
         /// <summary>
         /// Creates a new instance of <seealso cref="StringLicenseValidator"/>
         /// </summary>
